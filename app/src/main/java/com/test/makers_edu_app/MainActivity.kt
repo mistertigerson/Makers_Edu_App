0540fd578f26a8bcf6bdd2a543e1d20d5dba535c
mistertigerson <kg.ukr1996@gmail.com>
2022-03-27 15:39:34 +0600

    first commit

---
+++ app/src/main/java/com/test/makers_edu_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.makers_edu_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
