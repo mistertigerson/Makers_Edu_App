2077fd64a541547e150499661bf466bb6aa2a97b
mistertigerson <kg.ukr1996@gmail.com>
2022-03-30 20:45:52 +0600

    first commit

--- app/src/main/java/com/test/makers_edu_app/presentation/ui/fragments/details/DetailsFragment.kt
+++ app/src/main/java/com/test/makers_edu_app/presentation/ui/fragments/details/DetailsFragment.kt
@@ -1,25 +1,37 @@
 package com.test.makers_edu_app.presentation.ui.fragments.details
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.makersproject.presentation.ui.fragments.main.MainModel
+import com.test.makers_edu_app.R
 import com.test.makers_edu_app.databinding.FragmentDetailsBinding
+import com.test.makers_edu_app.presentation.ui.fragments.main.MainFragment.Companion.TITLE
 
 
-class DetailsFragment : Fragment() {
+class DetailsFragment : Fragment(R.layout.fragment_details) {
 
     private val binding: FragmentDetailsBinding by viewBinding()
-   // private val args: DetailsFragmentArgs by navArgs()
+    private lateinit var model : MainModel
+
+    // private val args: DetailsFragmentArgs by navArgs()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-     //   val name: String = args.title.toString()
-        val s = arguments?.getString("title")
-        binding.tvTitle.text = s
-        Log.e("TAG", "onViewCreated: ${s.toString()}", )
+        model = arguments?.get("title") as MainModel
+        getArgs()
+
+
+    }
+
+
+    fun getArgs() = with(binding) {
+        binding.tvTitle.text = model.title
+        binding.ivIcon.setImageResource(model.imageIcon)
+        binding.tvNameOfAuthor.text = model.NameOfAuthor
+        binding.tvComments.text = model.comments
 
     }
 }
--- app/src/main/java/com/test/makers_edu_app/presentation/ui/fragments/main/MainFragment.kt
+++ app/src/main/java/com/test/makers_edu_app/presentation/ui/fragments/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.test.makers_edu_app.presentation.ui.fragments.main
 
+import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -16,52 +17,53 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     private lateinit var adapter: MainAdapter
     private var list: ArrayList<MainModel> = ArrayList()
     private lateinit var mainModel: MainModel
+    private lateinit var mainModel2: MainModel
 
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e(ContentValues.TAG, "onCreate: ")
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val name: String? =  args.name
-//        val pass : String? =  args.password
         mainModel = MainModel(
             R.drawable.ic_launcher_background,
-           "relax",
-           "take it easy",
+            "relax",
+            "take it easy",
             "ahhahahah"
         )
-
+        mainModel2 = MainModel(
+            R.drawable.ic_add_photo,
+            "JAva",
+            "USA",
+            "AUF"
+        )
         list.add(mainModel)
-        list.add(mainModel)
-        list.add(mainModel)
-        list.add(mainModel)
+        list.add(mainModel2)
         adapter = MainAdapter(object : MainAdapter.ClickOnPlaylist2 {
             override fun onClick(model: MainModel, position: Int) {
-                val bundle: Bundle = Bundle()
-                val bundle2: Bundle = Bundle()
-
-                MainFragmentDirections.actionMainFragmentToDetailsFragment(list[position].title)
-                bundle.putSerializable(TITLE, list[position].title)
-                bundle2.putString("sexy", list[position].title)
-//                Log.e("TAG", "list: ${bundle2.getString("sexy")}")
-                Log.e("TAG", "list: ${MainFragmentDirections.actionMainFragmentToDetailsFragment(list[position].title)}")
-//                bundle.putSerializable("author", list[position].NameOfAuthor)
-//                bundle.putSerializable("key", list[position].comments)
+                val bundle = Bundle()
+                bundle.putSerializable("title", list[position])
                 findNavController().navigate(R.id.detailsFragment, bundle)
             }
 
             override fun clickBtn() {
                 findNavController().navigate(R.id.authorizationFragment)
             }
+
         })
         binding.recyclerMain.adapter = adapter
         adapter.setList(list)
 
+
+
+
+
     }
 
-    companion object{
+    companion object {
         const val TITLE = "title"
-        const val AUTHOR = "author"
-        const val COMMENTS = "comments"
-
     }
 }
